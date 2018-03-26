9a6b4483fe981b0ab15c70b39a9738e00024dc9f
AndersDLund <32234005+AndersDLund@users.noreply.github.com>
2018-03-26 18:07:59 -0500

    styling and slider functionality

--- accidental-iOS/accidental-iOS/DetailsViewController.swift
+++ accidental-iOS/accidental-iOS/DetailsViewController.swift
@@ -36,30 +36,185 @@ class DetailsViewController: UIViewController, StatusController {
     
     @IBOutlet weak var carImage: UIImageView!
     @IBOutlet weak var carLabel: UILabel!
-    @IBOutlet weak var damageReportLabel: TOMSMorphingLabel!
+   
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
     
     @IBOutlet weak var scratchLabel: TOMSMorphingLabel!
     @IBOutlet weak var dentLabel: TOMSMorphingLabel!
     @IBOutlet weak var chipLabel: TOMSMorphingLabel!
     @IBOutlet weak var curbLabel: TOMSMorphingLabel!
-    
-    @IBOutlet weak var scratchButton: UIButton!
-    @IBOutlet weak var dentButton: UIButton!
-    @IBOutlet weak var chipButton: UIButton!
-    @IBOutlet weak var curbButton: UIButton!
+
     
     @IBOutlet weak var scratchSlider: UISlider!
     @IBOutlet weak var dentSlider: UISlider!
     @IBOutlet weak var chipSlider: UISlider!
     @IBOutlet weak var curbSlider: UISlider!
     
+    @IBOutlet weak var scratchConfirmation: UIButton!
+    @IBOutlet weak var dentsConfirmation: UIButton!
+    @IBOutlet weak var chipsConfirmation: UIButton!
+    @IBOutlet weak var curbConfirmation: UIButton!
+    
+    
     var scratchCount = 0 //1
     var dentCount = 0 //2
     var chipCount = 0 //3
     var curbCount = 0 //4
     
+    @IBAction func scratchConfirmationClicked(_ sender: Any) {
+        let params = ["damage_type_id": 1]
+        if Int(scratchSlider.value) - scratchCount > 0 {
+            for _ in 0..<Int(scratchSlider.value) - scratchCount {
+                
+                Alamofire.request("https://aqueous-hollows-24814.herokuapp.com/damageNew/\(car!.car_id)", method: .post, parameters: params, encoding: JSONEncoding.default).responseString
+                    {response in
+//                        HUD.flash(.success, delay: 1.0)
+                        print(response)
+                }
+            }
+            HUD.flash(.success, delay: 1.0)
+        } else if Int(scratchSlider.value) - scratchCount < 0 {
+            //decrease
+//            for _ in 0..<abs(Int(scratchSlider.value) - scratchCount) {
+            
+                Alamofire.request("https://aqueous-hollows-24814.herokuapp.com/damageDelete/\(car!.car_id)?limit=\(abs(Int(scratchSlider.value) - scratchCount))", method: .delete, parameters: params, encoding: JSONEncoding.default).responseString
+                    {response in
+//                        HUD.flash(.success, delay: 1.0)
+                        print(response)
+                }
+            for _ in 0..<scratchCount - abs(Int(scratchSlider.value) - scratchCount) {
+                Alamofire.request("https://aqueous-hollows-24814.herokuapp.com/damageNew/\(car!.car_id)", method: .post, parameters: params, encoding: JSONEncoding.default).responseString
+                    {response in
+                        //                        HUD.flash(.success, delay: 1.0)
+                        print(response)
+                }
+            }
+            
+//            }
+            HUD.flash(.success, delay: 1.0)
+        } else {
+            //do nothing
+            print("nothing shoud change")
+        }
+        HUD.flash(.success, delay: 1.0)
     
-
+    }
+    
+    @IBAction func dentConfirmationClicked(_ sender: Any) {
+        let params = ["damage_type_id": 2]
+        if Int(dentSlider.value) - dentCount > 0 {
+            for _ in 0..<Int(dentSlider.value) - dentCount {
+                
+                Alamofire.request("https://aqueous-hollows-24814.herokuapp.com/damageNew/\(car!.car_id)", method: .post, parameters: params, encoding: JSONEncoding.default).responseString
+                    {response in
+                        //                        HUD.flash(.success, delay: 1.0)
+                        print(response)
+                }
+            }
+            HUD.flash(.success, delay: 1.0)
+        } else if Int(dentSlider.value) - dentCount < 0 {
+            //decrease
+            //            for _ in 0..<abs(Int(scratchSlider.value) - scratchCount) {
+            
+            Alamofire.request("https://aqueous-hollows-24814.herokuapp.com/damageDelete/\(car!.car_id)?limit=\(abs(Int(dentSlider.value) - dentCount))", method: .delete, parameters: params, encoding: JSONEncoding.default).responseString
+                {response in
+                    //                        HUD.flash(.success, delay: 1.0)
+                    print(response)
+            }
+            for _ in 0..<dentCount - abs(Int(dentSlider.value) - dentCount) {
+                Alamofire.request("https://aqueous-hollows-24814.herokuapp.com/damageNew/\(car!.car_id)", method: .post, parameters: params, encoding: JSONEncoding.default).responseString
+                    {response in
+                        //                        HUD.flash(.success, delay: 1.0)
+                        print(response)
+                }
+            }
+            
+            //            }
+            HUD.flash(.success, delay: 1.0)
+        } else {
+            //do nothing
+            print("nothing shoud change")
+        }
+        HUD.flash(.success, delay: 1.0)
+    }
+    
+    @IBAction func chipConfirmationClicked(_ sender: Any) {
+        let params = ["damage_type_id": 3]
+        if Int(chipSlider.value) - chipCount > 0 {
+            for _ in 0..<Int(chipSlider.value) - chipCount {
+                
+                Alamofire.request("https://aqueous-hollows-24814.herokuapp.com/damageNew/\(car!.car_id)", method: .post, parameters: params, encoding: JSONEncoding.default).responseString
+                    {response in
+                        //                        HUD.flash(.success, delay: 1.0)
+                        print(response)
+                }
+            }
+            HUD.flash(.success, delay: 1.0)
+        } else if Int(chipSlider.value) - chipCount < 0 {
+            //decrease
+            //            for _ in 0..<abs(Int(scratchSlider.value) - scratchCount) {
+            
+            Alamofire.request("https://aqueous-hollows-24814.herokuapp.com/damageDelete/\(car!.car_id)?limit=\(abs(Int(chipSlider.value) - chipCount))", method: .delete, parameters: params, encoding: JSONEncoding.default).responseString
+                {response in
+                    //                        HUD.flash(.success, delay: 1.0)
+                    print(response)
+            }
+            for _ in 0..<chipCount - abs(Int(chipSlider.value) - chipCount) {
+                Alamofire.request("https://aqueous-hollows-24814.herokuapp.com/damageNew/\(car!.car_id)", method: .post, parameters: params, encoding: JSONEncoding.default).responseString
+                    {response in
+                        //                        HUD.flash(.success, delay: 1.0)
+                        print(response)
+                }
+            }
+            
+            //            }
+            HUD.flash(.success, delay: 1.0)
+        } else {
+            //do nothing
+            print("nothing shoud change")
+        }
+        HUD.flash(.success, delay: 1.0)
+    }
+    
+    @IBAction func curbConfirmationClicked(_ sender: Any) {
+        let params = ["damage_type_id": 4]
+        if Int(curbSlider.value) - curbCount > 0 {
+            for _ in 0..<Int(curbSlider.value) - curbCount {
+                
+                Alamofire.request("https://aqueous-hollows-24814.herokuapp.com/damageNew/\(car!.car_id)", method: .post, parameters: params, encoding: JSONEncoding.default).responseString
+                    {response in
+                        //                        HUD.flash(.success, delay: 1.0)
+                        print(response)
+                }
+            }
+            HUD.flash(.success, delay: 1.0)
+        } else if Int(curbSlider.value) - curbCount < 0 {
+            //decrease
+            //            for _ in 0..<abs(Int(scratchSlider.value) - scratchCount) {
+            
+            Alamofire.request("https://aqueous-hollows-24814.herokuapp.com/damageDelete/\(car!.car_id)?limit=\(abs(Int(curbSlider.value) - curbCount))", method: .delete, parameters: params, encoding: JSONEncoding.default).responseString
+                {response in
+                    //                        HUD.flash(.success, delay: 1.0)
+                    print(response)
+            }
+            for _ in 0..<curbCount - abs(Int(curbSlider.value) - curbCount) {
+                Alamofire.request("https://aqueous-hollows-24814.herokuapp.com/damageNew/\(car!.car_id)", method: .post, parameters: params, encoding: JSONEncoding.default).responseString
+                    {response in
+                        //                        HUD.flash(.success, delay: 1.0)
+                        print(response)
+                }
+            }
+            
+            //            }
+            HUD.flash(.success, delay: 1.0)
+        } else {
+            //do nothing
+            print("nothing shoud change")
+        }
+        HUD.flash(.success, delay: 1.0)
+    }
+    
+    
 
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -82,54 +237,76 @@ class DetailsViewController: UIViewController, StatusController {
     }
     
     
-    
-
-    
-    
-    @IBAction func scratchButtonClicked(_ sender: Any) {
-        scratchSlider.isHidden = false
-        dentSlider.isHidden = true
-        chipSlider.isHidden = true
-        curbSlider.isHidden = true
-        
-        
-        
+    @IBAction func indexChanged(_ sender: Any) {
+        switch segmentedControl.selectedSegmentIndex{
+        case 0:
+            scratchSlider.isHidden = false
+            dentSlider.isHidden = true
+            chipSlider.isHidden = true
+            curbSlider.isHidden = true
+            
+            scratchConfirmation.isHidden = false
+            dentsConfirmation.isHidden = true
+            chipsConfirmation.isHidden = true
+            curbConfirmation.isHidden = true
+            
+            scratchLabel.isHidden = false
+            dentLabel.isHidden = true
+            chipLabel.isHidden = true
+            curbLabel.isHidden = true
+            
+        case 1:
+            dentSlider.isHidden = false
+            scratchSlider.isHidden = true
+            chipSlider.isHidden = true
+            curbSlider.isHidden = true
+            
+            scratchConfirmation.isHidden = true
+            dentsConfirmation.isHidden = false
+            chipsConfirmation.isHidden = true
+            curbConfirmation.isHidden = true
+            
+            scratchLabel.isHidden = true
+            dentLabel.isHidden = false
+            chipLabel.isHidden = true
+            curbLabel.isHidden = true
+            
+        case 2:
+            chipSlider.isHidden = false
+            scratchSlider.isHidden = true
+            dentSlider.isHidden = true
+            curbSlider.isHidden = true
+            
+            scratchConfirmation.isHidden = true
+            dentsConfirmation.isHidden = true
+            chipsConfirmation.isHidden = false
+            curbConfirmation.isHidden = true
+            
+            scratchLabel.isHidden = true
+            dentLabel.isHidden = true
+            chipLabel.isHidden = false
+            curbLabel.isHidden = true
+            
+        case 3:
+            curbSlider.isHidden = false
+            scratchSlider.isHidden = true
+            dentSlider.isHidden = true
+            chipSlider.isHidden = true
+            
+            scratchConfirmation.isHidden = true
+            dentsConfirmation.isHidden = true
+            chipsConfirmation.isHidden = true
+            curbConfirmation.isHidden = false
+            
+            scratchLabel.isHidden = true
+            dentLabel.isHidden = true
+            chipLabel.isHidden = true
+            curbLabel.isHidden = false
+            
+        default:
+            print("how did this happen?!?!?!")
+        }
     }
-
-    @IBAction func dentButtonClicked(_ sender: Any) {
-        dentSlider.isHidden = false
-        scratchSlider.isHidden = true
-        chipSlider.isHidden = true
-        curbSlider.isHidden = true
-        
-
-
-    }
-    
-    
-    @IBAction func chipButtonClicked(_ sender: Any) {
-        chipSlider.isHidden = false
-        scratchSlider.isHidden = true
-        dentSlider.isHidden = true
-        curbSlider.isHidden = true
-        
-   
-        
-    }
-    
-    @IBAction func curbButtonClicked(_ sender: Any) {
-        curbSlider.isHidden = false
-        scratchSlider.isHidden = true
-        dentSlider.isHidden = true
-        chipSlider.isHidden = true
-        
-        
-      
-    }
-    
-    
-    
-    
     
     
     override func viewDidLoad() {
@@ -138,6 +315,16 @@ class DetailsViewController: UIViewController, StatusController {
         dentSlider.isHidden = true
         chipSlider.isHidden = true
         curbSlider.isHidden = true
+        
+        scratchLabel.isHidden = true
+        dentLabel.isHidden = true
+        chipLabel.isHidden = true
+        curbLabel.isHidden = true
+        
+        scratchConfirmation.isHidden = true
+        dentsConfirmation.isHidden = true
+        chipsConfirmation.isHidden = true
+        curbConfirmation.isHidden = true
         
         
         
@@ -148,11 +335,6 @@ class DetailsViewController: UIViewController, StatusController {
         
         carImage.image = UIImage(data:data!)
         
-        
-        self.scratchButton.setTitle("Scratchs:", for: .normal)
-        self.dentButton.setTitle("Dents:", for: .normal)
-        self.chipButton.setTitle("Chips:", for: .normal)
-        self.curbButton.setTitle("Curb Rash:", for: .normal)
         
         self.scratchLabel.text = "\(0)"
         self.dentLabel.text = "\(0)"
@@ -169,10 +351,6 @@ class DetailsViewController: UIViewController, StatusController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
 
-        let status = Status(title: "🙅‍♀️No Damage Recorded🙅‍♀️", description: "Keep up the Good Work!", image: UIImage(named: "placeholder")) {
-            self.hideStatus()
-           
-        }
         
         print(self.car!, "the car!")
         Alamofire.request("https://aqueous-hollows-24814.herokuapp.com/damageGet/\(car!.car_id)", method: .get, encoding: JSONEncoding.default).responseSwiftyJSON
@@ -216,32 +394,33 @@ class DetailsViewController: UIViewController, StatusController {
                         }
                         self.curbSlider.value = Float(self.curbCount)
                         self.curbSlider.minimumValue = 0
-                        self.curbSlider.maximumValue = Float(self.curbCount + 10)
+                        self.curbSlider.maximumValue = Float(self.curbCount + 5)
                         
                         self.chipSlider.value = Float(self.chipCount)
                         self.chipSlider.minimumValue = 0
-                        self.chipSlider.maximumValue = Float(self.chipCount + 10)
+                        self.chipSlider.maximumValue = Float(self.chipCount + 5)
                         
                         self.dentSlider.value = Float(self.dentCount)
                         self.dentSlider.minimumValue = 0
-                        self.dentSlider.maximumValue = Float(self.dentCount + 10)
+                        self.dentSlider.maximumValue = Float(self.dentCount + 5)
                         
                         self.scratchSlider.value = Float(self.scratchCount)
                         self.scratchSlider.minimumValue = 0
-                        self.scratchSlider.maximumValue = Float(self.scratchCount + 10)
+                        self.scratchSlider.maximumValue = Float(self.scratchCount + 5)
                     }
                 case .failure:
                     print("no damage")
-                    self.scratchLabel.isHidden = true
-                    self.dentLabel.isHidden = true
-                    self.chipLabel.isHidden = true
-                    self.curbLabel.isHidden = true
-                    self.damageReportLabel.isHidden = true
-                    self.scratchButton.isHidden = true
-                    self.dentButton.isHidden = true
-                    self.chipButton.isHidden = true
-                    self.curbButton.isHidden = true
-                    self.show(status:status)
+//                    self.scratchLabel.isHidden = true
+//                    self.dentLabel.isHidden = true
+//                    self.chipLabel.isHidden = true
+//                    self.curbLabel.isHidden = true
+//
+//
+//                    self.scratchConfirmation.isHidden = true
+//                    self.dentsConfirmation.isHidden = true
+//                    self.chipsConfirmation.isHidden = true
+//                    self.curbConfirmation.isHidden = true
+                    
                 }
         }
     
